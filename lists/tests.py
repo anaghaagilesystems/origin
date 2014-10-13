a08abf86d9b8eff337fdb148ce79b39db408b14b
Jayprakash <jayprakashthontadarya@live.com>
2014-10-13 19:50:45 +0530

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -15,3 +15,9 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_return_correct_thml(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
