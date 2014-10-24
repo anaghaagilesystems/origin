3bfecf241ed712c1a579faa88ecbcd4b00e73f94
Jayprakash <jayprakashthontadarya@live.com>
2014-10-24 22:29:27 +0530

    Added validate uniqueness of constraints of models.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,7 +7,7 @@ from django.utils.html import escape
 from lists.models import Item, List
 from lists.views import home_page
 from lists.forms import EMPTY_LIST_ERROR, ItemForm
-
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -141,7 +141,18 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
 
-
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
